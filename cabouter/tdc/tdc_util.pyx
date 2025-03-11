87a0e4addff2a2a31ef9785c0bf50132af3019e1
Emiel <emailmonteur@gmail.com>
2025-03-11 14:59:15 +0100

    fixed strings

--- cabouter/tdc/tdc_util.pyx
+++ cabouter/tdc/tdc_util.pyx
@@ -1,11 +1,20 @@
 # import yaml
 from .xhptdc8_util cimport xhptdc8_apply_yaml
-from .xhptdc8_interface cimport xhptdc8_manager_configuration, xhptdc8_get_default_configuration, xhptdc8_configure
+from .xhptdc8_interface cimport xhptdc8_manager_configuration, xhptdc8_get_default_configuration, xhptdc8_configure, xhptdc8_get_last_error_message, XHPTDC8_OK
+from libc.stdio cimport printf
 
-def apply_yaml_manager_configuration(yaml_str: str):
+cdef catch_errs(int status, const char *message):
+    if status == XHPTDC8_OK:
+        return status
+    
+    printf("TDC ERROR: %s: %s\n", message, xhptdc8_get_last_error_message(0))
+    return
 
-    cdef xhptdc8_manager_configuration *cfg
-    xhptdc8_get_default_configuration(cfg)
-    xhptdc8_apply_yaml(cfg, yaml_str)
-    xhptdc8_configure(cfg)
+def apply_yaml_manager_configuration(yaml_str: bytes):
+
+    cdef xhptdc8_manager_configuration cfg
+    cdef char *yaml_string = yaml_str
+    xhptdc8_get_default_configuration(&cfg)
+    xhptdc8_apply_yaml(&cfg, yaml_string)
+    xhptdc8_configure(&cfg)
     
--- tests/hello.py
+++ tests/hello.py
@@ -9,12 +9,15 @@ from cabouter.tdc.driver_interface import (
 from cabouter.tdc.tdc_util import apply_yaml_manager_configuration
 
 # print(get_default_configuration())
-initialize_tdc(1000)
+initialize_tdc(8 * 1024 * 1024)
+print(f"grouping enabled is {get_grouping()}")
+print(f"grouping enabled is {get_grouping()}")
 print(f"grouping enabled is {get_grouping()}")
 with open("tests/config.yaml", "r") as f:
     config_yaml = yaml.load(f, yaml.CLoader)
 
 print("Attempting to set config...")
-apply_yaml_manager_configuration(yaml.dump(config_yaml))
+apply_yaml_manager_configuration(yaml.dump(config_yaml, encoding="utf-8"))
+print(f"grouping enabled is {get_grouping()}")
 print(f"grouping enabled is {get_grouping()}")
 close_tdc()
