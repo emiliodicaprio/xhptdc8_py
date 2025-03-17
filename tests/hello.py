bd622caa103ab243e055c2e16797facc0939bcb2
Emiel de Wit <emiel.de.wit@rug.nl>
2025-03-17 17:04:05 +0100

    whatever

--- tests/hello.py
+++ tests/hello.py
@@ -1,9 +1,10 @@
 import yaml
 
-from xhptdc8_py.tdc import close, device_state_to_str, get_fast_info, initialize_tdc
+from xhptdc8_py.tdc import close, device_state_to_str, get_static_info, initialize_tdc
 
 # print(get_default_configuration())
 initialize_tdc(8 * 1024 * 1024)
-state = get_fast_info(0).state
-print(device_state_to_str(state))
+state = get_static_info(0).flash_valid
+# print(device_state_to_str(state))
+print(state)
 close()
