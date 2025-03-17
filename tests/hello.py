c3870183865d2e2bb1aec3cacac939151f48cfec
Emiel de Wit <emiel.de.wit@rug.nl>
2025-03-17 15:02:20 +0100

    update stubs

--- tests/hello.py
+++ tests/hello.py
@@ -1,17 +1,8 @@
 import yaml
 
-from xhptdc8_py.tdc import *
-from xhptdc8_py.util import apply_yaml_manager_configuration
+from xhptdc8_py.tdc import close, get_static_info, initialize_tdc
 
 # print(get_default_configuration())
 initialize_tdc(8 * 1024 * 1024)
-# print(f"grouping enabled is {get_grouping()}")
-# print(f"grouping enabled is {get_grouping()}")
-# print(f"grouping enabled is {get_grouping()}")
-with open("tests/config.yaml", "r") as f:
-    config_yaml = yaml.load(f, yaml.CLoader)
-
-print("Attempting to set config...")
-apply_yaml_manager_configuration(yaml.dump(config_yaml, encoding="utf-8"))
-# print(f"grouping enabled is {get_grouping()}")
+print(get_static_info(0).chip_id)
 close()
--- xhptdc8_py/tdc.pyx
+++ xhptdc8_py/tdc.pyx
@@ -2,6 +2,7 @@ cimport cython
 from .xhptdc8_interface cimport *
 from .crono_interface cimport *
 from libc.stdio cimport printf
+from libc.stdlib cimport free
 
 cpdef py_print_error():
     return
@@ -75,6 +76,9 @@ def software_trigger(index: int) -> None:
 
 cdef class Static_info:
     # cdef xhptdc8_static_info info
+    """Structure containing static information about the board that does not change during run time. 
+    It is provided by the function get_static_info()
+    """
     cdef xhptdc8_static_info info
 
     def __init__(self):
@@ -82,57 +86,93 @@ cdef class Static_info:
     
     @property
     def version(self) -> int:
+        """A version number which increases when the definition of the structure is changed"""
         return self.info.version
     
     @property
     def board_id(self) -> int:
+        """ID of the board. All xHPTDC8-PCIe boards in the system are numbered in order of serial number,
+        starting at zero."""
         return self.info.board_id
     
     @property
     def driver_revision(self) -> int:
+        """Encoded version number for the driver.
+        The lower three bytes contain a triple level hierarchy of version
+        numbers. E.g. 0x010103 codes version 1.1.3.
+        
+        A change in the first digit generally requires a recompilation of
+        user applications. Change in the second digit denote significant
+        improvements or changes that don't break compatibility and the third
+        digit changes with minor bugfixes and the like."""
         return self.info.board_revision
     
     @property
     def driver_build_revision(self) -> int:
+        """The build number of the driver according to cronologic's internal versioning system."""
         return self.info.driver_build_revision
 
     @property
     def firmware_revision(self) -> int:
+        """Revision number of the FPGA configuration."""
         return self.info.firmware_revision
     
     @property
     def board_revision(self) -> int:
+        """Board revision number.
+        The board revision number can be read from a register. It is a four
+        bit number that changes when the schematic of the board is changed.
+        - 0: Experimental first board Version. Labeled "Rev. 1"
+        - 2: First commercial Version. Labeled "Rev. 2"
+        """
         return self.info.board_revision
     
     @property
     def board_configuration(self) -> int:
+        """Describes the schematic configuration of the board.
+
+        The same board schematic can be populated in multiple variants. This is a eight bit-code that can be read from a register.
+        """
         return self.info.board_configuration
     
     @property
     def subversion_revision(self) -> int:
+        """Subversion revision id of the FPGA configuration.
+        A number to track builds of the firmware in more detail than the firmware revision. It changes with every change in the firmware, even if there is no visible effect for the user. The subversion revision number can be read from a register."""
         return self.info.subversion_revision
     
     @property
     def chip_id(self) -> list:
+        """16bit factory ID of the TDC chip.
+        
+        This is the chipID as read from the 16 bit TDC chip id register."""
         return self.info.chip_id
 
     @property
     def board_serial(self) -> int:
+        """Serial number
+
+        With year and number running in 8.24 format. The number is identical to the one printed on the silvery sticker on the board.
+        
+        """
         return self.info.board_serial
     
     @property
     def flash_serial_high(self) -> int:
+        """High 32 bits of 64 bit manufacturer serial number of the flash chip."""
         return self.info.flash_serial_high
 
     @property
     def flash_serial_low(self) -> int:
+        """Low 32 bits of 64 bit manufacturer serial number of the flash chip."""
         return self.info.flash_serial_low
     
     @property
     def flash_valid(self) -> bool:
+        """Calibration data read from flash is valid.
+        
+        If not 'False', the driver found valid calibration data in the flash on the board and is using it."""
         return self.info.flash_valid
-    
-
 
 def get_static_info(index: int) -> None:
     """Returns static information about the device.
