b8b99343f013348be00caa8920a7c450b9b7965a
Emiel de Wit <emailmonteur@gmail.com>
2025-05-20 10:00:19 +0200

    changes for building

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     name="xhptdc8_py",
     ext_modules=cythonize(extensions, include_path=["./xhptdc8_py"], build_dir="build"),
     package_dir={"xhptdc8_py": "xhptdc8_py"},
-    package_data={"xhptdc8_py": ["tdc.pyi"]},
+    package_data={"xhptdc8_py": ["tdc.pyi", "xhptdc8_babel/**"]},
     include_dirs=["./xhptdc8_py", "./lib"],
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
--- tests/hello.py
+++ tests/hello.py
@@ -6,10 +6,19 @@ from xhptdc8_py.tdc import (
     get_default_init_parameters,
     get_fast_info,
     init_xhptdc8,
+    start_capture,
 )
+from xhptdc8_py.util import apply_yaml_manager_configuration
 
-# initialize_tdc(1024 * 200)
+with open("tests/config.yaml", "rb") as f:
+    mgr_config_yaml = f.read()
+
 print(f"Device state before init: {device_state_to_str(get_fast_info(0).state)}")
 params = get_default_init_parameters()
 init_xhptdc8(params)
+print(f"Device state after init: {device_state_to_str(get_fast_info(0).state)}")
+apply_yaml_manager_configuration(mgr_config_yaml)
+print(
+    f"Device state after manager_config: {device_state_to_str(get_fast_info(0).state)}"
+)
 close()
