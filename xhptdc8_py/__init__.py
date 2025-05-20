8ed67917bbb5c5ac7a1f86059473d39e11fb9f5a
Emiel de Wit <emailmonteur@gmail.com>
2025-05-20 16:56:27 +0200

    add preloading dlls

--- xhptdc8_py/__init__.py
+++ xhptdc8_py/__init__.py
@@ -2,5 +2,7 @@ import ctypes
 import os
 
 dirname = os.path.dirname(__file__)
-dll_fp = os.path.join(dirname, "xhptdc8_babel/bin/xhptdc8_driver_64.dll")
-ctypes.CDLL(dll_fp)
+driver_dll_fp = os.path.join(dirname, "xhptdc8_babel/bin/xhptdc8_driver_64.dll")
+util_dll_fp = os.path.join(dirname, "xhptdc8_babel/bin/xhptdc8_util.dll")
+ctypes.CDLL(driver_dll_fp)
+ctypes.CDLL(util_dll_fp)
